953d272a91b032ff328de5e1f9eb67305950793a
ZapAnton <wc3ft2@gmail.com>
2018-05-04 03:26:10 +0300

    Getting rss addresses from config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate reqwest;
+
+use std::{fs::File, io::{BufRead, BufReader}};
+
+fn get_rss_addresses() -> Result<Vec<String>, std::io::Error> {
+    let rss_file_path = "config/rss.txt";
+
+    let rss_file = File::open(&rss_file_path)?;
+
+    let rss_addrs = BufReader::new(rss_file)
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+
+    Ok(rss_addrs)
+}
+
+fn get_news() {
+    let rss_addrs = get_rss_addresses();
+
+    println!("{:?}", rss_addrs);
+}
+
 fn main() {
-    println!("Hello, world!");
+    get_news();
 }
